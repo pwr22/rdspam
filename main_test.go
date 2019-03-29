413dbf8be336dec588fc3095cb042f2a1e6b4eb6
Peter Roberts <me@peter-r.co.uk>
2019-03-29 23:58:39 +0000

    Add some more benchmarks

--- main_test.go
+++ main_test.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
+	"github.com/c2h5oh/datasize"
 	"github.com/kbjorklu/xoshiro"
 )
+
+func BenchmarkXoshiro64Bits(b *testing.B) {
+	rndSrc := xoshiro.NewXoshiro256StarStar(11)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		rndSrc.Uint64()
+	}
+}
 
 func BenchmarkGenChunk(b *testing.B) {
 	buf := make([]byte, bufLen)
@@ -13,5 +25,24 @@ func BenchmarkGenChunk(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		genDataChunk(buf, rndSrc)
+	}
+}
+
+func BenchmarkWriteDataDiscar(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		writeData(int(4*datasize.MB), 11, ioutil.Discard)
+	}
+}
+
+func BenchmarkWriteDataDevNull(b *testing.B) {
+	out, err := os.Create(os.DevNull)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer out.Close()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		writeData(int(4*datasize.MB), 11, out)
 	}
 }
