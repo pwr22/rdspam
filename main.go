b957e7a6de0542f5b00e2396456805b0a1e1fde4
Peter Roberts <me@peter-r.co.uk>
2019-03-29 19:55:06 +0000

    Add an optional --seed
    By default we seed from the time

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/c2h5oh/datasize"
 	"github.com/kbjorklu/xoshiro"
@@ -14,7 +16,9 @@ const version = "v0.0.1"
 
 var size = flag.IntP("size", "s", 0, "number of bytes to write or 0 to keep going forever")
 var printVersion = flag.BoolP("version", "V", false, "print version information")
+var seed = flag.Int64P("seed", "S", 0, "seed to use for the data source (defaults to the current time)")
 
+// TODO optimise for different output types?
 const bufLen = 64 * datasize.KB // optimised for piping
 
 // main will parse flags, do anything needed there then start writing out data as requested.
@@ -33,17 +37,23 @@ func main() {
 		os.Exit(2)
 	}
 
+	if *seed == 0 { // if not set or user gave 0 - xoroshift doesn't like 0
+		rand.Seed(time.Now().UnixNano())
+		*seed = int64(rand.Uint64())
+	}
+
 	writeData()
 }
 
 // writeData writes the requested amount of random data to stdout then returns.
 // If *size == 0 then it will keep generating forever and never return.
 func writeData() {
+	rndSrc := xoshiro.NewXoshiro256StarStar(*seed)
 	buf := make([]byte, bufLen)
 	bytesWritten := 0
 
 	for {
-		genDataChunk(buf)
+		genDataChunk(buf, rndSrc)
 
 		// handle the last write potentially being smaller and exit
 		if *size > 0 && *size-bytesWritten <= len(buf) {
@@ -56,12 +66,8 @@ func writeData() {
 	}
 }
 
-// r is the source of randomness used for data generation.
-// TODO Allow this to be seeded.
-var r = xoshiro.NewXoshiro256StarStar(99)
-
 // genDataChunk generates the next chunk of random data in c.
-func genDataChunk(c []byte) {
+func genDataChunk(c []byte, r *xoshiro.Xoshiro256StarStar) {
 	for i := 0; i < len(c); i += 8 {
 		binary.LittleEndian.PutUint64(c[i:], r.Uint64())
 	}
